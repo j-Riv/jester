3f8ccdb54901a845705af45f4674d81e19d83366
j-Riv <jose.alfredo.rivera@gmail.com>
2019-04-16 00:08:34 -0700

    EOD

--- client/src/containers/Room/Room.js
+++ client/src/containers/Room/Room.js
@@ -48,6 +48,7 @@ class Game extends Component {
     }
 
     componentDidMount = () => {
+        console.log('Host: ' + host);
         const { match: { params } } = this.props;
         // get gifs
         const word = words.words[~~(Math.random() * words.words.length)];
