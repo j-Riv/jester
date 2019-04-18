a465f1d9be48de780048eca2b3ed81ae8458c467
j-Riv <jose.alfredo.rivera@gmail.com>
2019-04-17 21:53:13 -0700

    Reducer cleanup

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,5 @@
 export const AUTH_USER = 'auth_user';
 export const AUTH_ERROR = 'auth_error';
-export const SET_COLOR = 'SET_COLOR';
 export const ADD_CHAT = 'ADD_CHAT';
 export const CURRENT_USER = 'CURRENT_USER';
 export const CURRENT_GAME = 'CURRENT_GAME';
