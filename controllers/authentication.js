fac2713542b4a6dd36f3717568d4783eaa0ce1de
j-Riv <jriv@suavecito.com>
2019-04-19 09:27:46 -0700

    Cleanup + ES6

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const axios = require('axios');
 const jwt = require('jwt-simple');
 const User = require('../models/user');
 const Game = require('../models/game');
@@ -9,13 +8,13 @@ function tokenForUser(user) {
     return jwt.encode({ sub: user.id, iat: timestamp }, process.env.PASSPORT_SECRET);
 }
 
-exports.signin = function (req, res, next) {
+exports.signin = (req, res) => {
     // User has already had their email and password auth'd
     // We just need to give them a token
     res.send({ token: tokenForUser(req.user), currentUser: req.user });
 }
 
-exports.signup = function (req, res, next) {
+exports.signup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const username = req.body.username;
@@ -25,7 +24,7 @@ exports.signup = function (req, res, next) {
     }
 
     // See if a user with the given email exists
-    User.findOne({ email: email }, function (err, existingUser) {
+    User.findOne({ email: email }, (err, existingUser) => {
         if (err) { return next(err); }
         // If a user with email does exist, return an error
         if (existingUser) {
@@ -38,7 +37,7 @@ exports.signup = function (req, res, next) {
             username: username
         });
 
-        user.save(function (err) {
+        user.save(err => {
             if (err) { return next(err); }
             // Repond to request indicating the user was created
             res.json({ token: tokenForUser(user), currentUser: user });
@@ -46,29 +45,37 @@ exports.signup = function (req, res, next) {
     });
 }
 
-exports.getCurrentUser = function (req, res, next) {
+exports.getCurrentUser = (req, res) => {
     const token = req.params.token;
     const decoded = jwt.decode(token, process.env.PASSPORT_SECRET);
-    User.findOne({ _id: decoded.sub }).then(function (result) {
+    User.findOne({ 
+        _id: decoded.sub 
+    }).then(result => {
         res.json({ currentUser: result });
     });
 }
 
-exports.updateUser = function (req, res, next) {
+exports.updateUser = (req, res) => {
     const id = req.body.id;
     const username = req.body.username;
     const picture = req.body.picture;
     console.log('this is the id: ' + id);
     console.log('this is the username: ' + username);
-    User.findOneAndUpdate({ _id: id }, { $set: { username: username, picture: picture }
-    }).then(function (result) {
+    User.findOneAndUpdate({ 
+        _id: id 
+    }, { 
+        $set: { 
+            username: username, 
+            picture: picture 
+        }
+    }).then(result => {
         res.json({ currentUser: result });
-    }).catch(function (error) {
+    }).catch(error => {
         console.log(error);
     });
 }
 
-exports.createGame = function (req, res, next) {
+exports.createGame = (req, res, next) => {
     // create new game
     const game = new Game({
         phrase: null,
@@ -82,7 +89,7 @@ exports.createGame = function (req, res, next) {
         category: req.body.game_category,
         status: req.body.game_status
     });
-    game.save(function(err, newGame) {
+    game.save((err, newGame) => {
         if (err) { return next(err); }
         // Repond to request indicating the game was created
         // Send new game object back
@@ -91,27 +98,27 @@ exports.createGame = function (req, res, next) {
     });
 }
 
-exports.getGame = function(req, res, next) {
+exports.getGame = (req, res) => {
     const id = req.params.id;
     // get game by id
-    Game.findById(id).then(function (result) {
+    Game.findById(id).then(result => {
         res.json({ game: result });
-    }).catch(function (error) {
+    }).catch(error => {
         console.log(error);
     });
 }
 
-exports.getAllGames = function(req, res, next) {
+exports.getAllGames = (req, res, next) => {
     // get all games for lobby update
-    Game.find({}).then(function (result) {
+    Game.find({}).then(result => {
         // all games currently in db
         res.json({ games: result });
-    }).catch(function (error) {
+    }).catch(error => {
         console.log(error);
     });
 }
 
-exports.updateGame = function(req, res, next) {
+exports.updateGame = (req, res) => {
     const id = req.params.id;
     const game = {
         title: req.body.title,
@@ -119,39 +126,59 @@ exports.updateGame = function(req, res, next) {
         // other props
     }
     // update game with matching id
-    Game.findOneAndUpdate({ _id: id }, { $set: { game } }).then(function (result) {
+    Game.findOneAndUpdate({ 
+        _id: id 
+    }, { 
+        $set: { 
+            game 
+        } 
+    }).then(result => {
         res.json({ updatedGame: result });
-    }).catch(function (error) {
+    }).catch(error => {
         console.log(error);
     });
 }
 
-exports.addUser = function (req, res, next) {
+exports.addUser = (req, res) => {
     const id = req.body.gameId;
     const user = req.body.user;
     const user_id = req.body.userId;
     if (user !== null) {
         // get user data then --->
         // updating game with new user
-        User.findOne({ _id: user_id }).then(function (userData) {
+        User.findOne({ 
+            _id: user_id 
+        }).then(userData => {
             // build user obj
             const userObj = {
                 _id: userData._id,
                 username: userData.username,
                 picture: userData.picture
             };
-            Game.findOneAndUpdate({ _id: id }, { $addToSet: { 'users': { user: user, wins: 0, data: userObj } } }, { new: true }).then(function (result) {
-                req.io.in(id).emit('add user', { user: user, wins: 0, data: userObj });
-                // user has been added
-                console.log(result.users);
-                // emit event to clients in lobby
-                req.io.emit('update games');
-                // send user data
-                res.json({ added: { user: user, wins: 0, data: userObj } });
-            }).catch(function (error) {
-                console.log(error);
-            });
-        }).catch(function (error) {
+            Game.findOneAndUpdate({ 
+                _id: id
+             }, { 
+                 $addToSet: { 
+                     'users': { 
+                         user: user, 
+                         wins: 0, 
+                         data: userObj 
+                        } 
+                    } 
+                }, { 
+                    new: true
+                }).then(result => {
+                    req.io.in(id).emit('add user', { user: user, wins: 0, data: userObj });
+                    // user has been added
+                    console.log(result.users);
+                    // emit event to clients in lobby
+                    req.io.emit('update games');
+                    // send user data
+                    res.json({ added: { user: user, wins: 0, data: userObj } });
+                }).catch(error => {
+                    console.log(error);
+                });
+        }).catch(error => {
             console.log(error);
         });
     }else{
@@ -159,13 +186,25 @@ exports.addUser = function (req, res, next) {
     }
 }
 
-exports.removeUser = function (req, res, next) {
+exports.removeUser = (req, res) => {
     const id = req.body.gameId;
     const user = req.body.user;
     const nextUser = req.body.nextUser;
     if (user !== null) {
         console.log(`Removing this user: ${user} Updating game: ${id}`);
-        Game.findOneAndUpdate({ _id: id }, { $pull: { 'users':  { user: user } } }, { safe: true, multi: true, new: true }).then(function (result) {
+        Game.findOneAndUpdate({ 
+            _id: id 
+        }, { 
+            $pull: { 
+                'users':  { 
+                    user: user 
+                } 
+            } 
+        }, { 
+            safe: true, 
+            multi: true, 
+            new: true 
+        }).then(result => {
             // remove user
             console.log('User ' + user + ' has been removed --->');
             console.log(result.users);
@@ -173,28 +212,42 @@ exports.removeUser = function (req, res, next) {
             req.io.in(id).emit('remove user', { user: user, nextUser: nextUser });
             // update current turn if game is empty
             if(result.users.length < 1) {
-                Game.findOneAndUpdate({ _id: id }, { $set: { current_turn: '' }, new: true }).then(function (result) {
+                Game.findOneAndUpdate({ 
+                    _id: id 
+                }, { 
+                    $set: { 
+                        current_turn: '' 
+                    }, 
+                    new: true 
+                }).then(result => {
                     // updated current turn to empty string once game was empty
                     console.log('updating current turn to empty on server ---->');
                     console.log(result);
-                }).catch(function (error) {
+                }).catch(error => {
                     console.log(error);
                 });
             }
             // update if current_turn (king) has left
             if (result.current_turn === user) {
-                Game.findOneAndUpdate({ _id: id }, { $set: { current_turn: nextUser }, new: true }).then(function (result) {
+                Game.findOneAndUpdate({ 
+                    _id: id 
+                }, { 
+                    $set: { 
+                        current_turn: nextUser 
+                    }, 
+                    new: true 
+                }).then(result => {
                     // updated current to run with next user
                     console.log('updating current turn to to next ' + nextUser + ' on server ---->');
                     console.log(result);
-                }).catch(function (error) {
+                }).catch(error => {
                     console.log(error);
                 });
             }
             // emit event to clients in lobby
             req.io.emit('update games');
             res.json({ removed: { user: user, nextUser: nextUser } });
-        }).catch(function (error) {
+        }).catch(error => {
             console.log(error);
         });
     } else {
@@ -202,30 +255,36 @@ exports.removeUser = function (req, res, next) {
     }
 }
 
-exports.updateCurrentTurn = function (req, res, next) {
+exports.updateCurrentTurn = (req, res) => {
     const id = req.body.gameId;
     const user = req.body.user;
     const phrase = req.body.phrase;
-    Game.findOneAndUpdate({ _id: id }, { $set: { current_turn: user, phrase: phrase }, new: true }).then(function (result) {
+    Game.findOneAndUpdate({ 
+        _id: id 
+    }, { 
+        $set: { 
+            current_turn: user, 
+            phrase: phrase 
+        }, 
+        new: true 
+    }).then(result => {
         // update current turn
         console.log('updating current turn ' + user + ' on server ---->');
         console.log(result);
         res.json({ turn: user, phrase: phrase });
-    }).catch(function (error) {
+    }).catch(error => {
         console.log(error);
     });
 }
 
-exports.updateGameCards = function (req, res, next) {
+exports.updateGameCards = (req, res) => {
     const id = req.body.gameId;
-    const user = req.body.user;
-    const card = req.body.card;
     // update room with card
     req.io.in(id).emit('update cards', req.body);
     res.json({ card: req.body });
 }
 
-exports.updateGameWinner = function (req, res, next) {
+exports.updateGameWinner = (req, res) => {
     const id = req.body.gameId;
     const user = req.body.user;
     const phrase = req.body.phrase;
