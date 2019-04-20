f626f7df543806910a513baecd46623476221937
Mikeyhey <mikeyhey@hotmail.com>
2019-04-20 12:24:31 -0700

    conf

--- client/src/components/Profile/ProfileBar.js
+++ client/src/components/Profile/ProfileBar.js
@@ -1,89 +1,89 @@
-import React, { Component } from 'react';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
-import requireAuth from '../../containers/requireAuth';
-import { slide as Menu } from 'react-burger-menu'
-import "./Profile.css";
-import ProAccord from './ProfileAccord';
-import Dropdown from 'react-bootstrap/Dropdown';
+// import React, { Component } from 'react';
+// import { compose } from 'redux';
+// import { connect } from 'react-redux';
+// import * as actions from '../../actions';
+// import requireAuth from '../../containers/requireAuth';
+// import { slide as Menu } from 'react-burger-menu'
+// import "./Profile.css";
+// import ProAccord from './ProfileAccord';
+// import Dropdown from 'react-bootstrap/Dropdown';
 
-// import ProfileModal from './ProfileModal';
-class Profile extends React.Component {
+// // import ProfileModal from './ProfileModal';
+// class Profile extends React.Component {
 
-    render() {
+//     render() {
 
-        const user = this.props.currentUser;
-        const total = user.wins + user.losses
-        let ratio = user.wins / user.losses
-        if (isNaN(ratio)) {
-            ratio = "Play some games"
-        }
+//         const user = this.props.currentUser;
+//         const total = user.wins + user.losses
+//         let ratio = user.wins / user.losses
+//         if (isNaN(ratio)) {
+//             ratio = "Play some games"
+//         }
 
 
-        return (
+//         return (
 
-            <Menu>
-                <div className="container-fluid pro">
-                    <ProAccord />
+//             <Menu id="proContainer">
+//                 <div className="container-fluid pro">
+//                     <ProAccord />
 
-                    <div className="row">
-                        <div className="col-6 text-center">
-                            <img className="img-fluid img-thumbnail rounded-circle mt-4  mx-auto d-block" id='proPic' src={user.picture} alt="profile picture" />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h1 className="text-center text" id="username" >{user.username}</h1>
-                        </div>
-                    </div>
-                    {/* math realted stuff */}
-                    <div className="row d-flex justify-content-center">
-                        <div className="col-3 text-center mr-5 mt-2">
-                            <h2 className="text">Ratio: {ratio}</h2>
-                        </div>
-                        <div className="col-3  text-center">
-                            <h2 className="text" id="gamesPlayed">Games Played: {total} </h2>
-                        </div>
-                    </div>
-                    {/* recent games */}
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h2 className="text-center text">Recent Games:</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-12 d-flex justify-content-center">
-                            <div className="row border ">
-                                <div className="col-4">
-                                    <img className="img-fluid img-thumbnail rounded-circle w-100" id="gamePic" src="https://i.imgur.com/AQKsp6n.jpg" alt="user" />
-                                </div>
-                                <div className="col-4 align-self-center text">
-                                    <p>User1</p>
-                                </div>
-                                <div className="col-4 align-self-center text">
-                                    <p>Win</p>
-                                </div>
-                            </div>
+//                     <div className="row">
+//                         <div className="col-6 text-center">
+//                             <img className="img-fluid img-thumbnail rounded-circle mt-4  mx-auto d-block" id='proPic' src={user.picture} alt="profile picture" />
+//                         </div>
+//                     </div>
+//                     <div className="row">
+//                         <div className="col-sm-12">
+//                             <h1 className="text-center text" id="username" >{user.username}</h1>
+//                         </div>
+//                     </div>
+//                     {/* math realted stuff */}
+//                     <div className="row d-flex justify-content-center">
+//                         <div className="col-3 text-center mr-5 mt-2">
+//                             <h2 className="text">Ratio: {ratio}</h2>
+//                         </div>
+//                         <div className="col-3  text-center">
+//                             <h2 className="text" id="gamesPlayed">Games Played: {total} </h2>
+//                         </div>
+//                     </div>
+//                     {/* recent games */}
+//                     <div className="row">
+//                         <div className="col-sm-12">
+//                             <h2 className="text-center text">Recent Games:</h2>
+//                         </div>
+//                     </div>
+//                     <div className="row">
+//                         <div className="col-sm-12 d-flex justify-content-center">
+//                             <div className="row border ">
+//                                 <div className="col-4">
+//                                     <img className="img-fluid img-thumbnail rounded-circle w-100" id="gamePic" src="https://i.imgur.com/AQKsp6n.jpg" alt="user" />
+//                                 </div>
+//                                 <div className="col-4 align-self-center text">
+//                                     <p>User1</p>
+//                                 </div>
+//                                 <div className="col-4 align-self-center text">
+//                                     <p>Win</p>
+//                                 </div>
+//                             </div>
 
-                        </div>
+//                         </div>
 
-                    </div>
+//                     </div>
 
-                </div >
+//                 </div >
 
-            </Menu>
+//             </Menu>
 
-        );
-    }
+//         );
+//     }
 
-}
+// }
 
-// export default Profile;
-function mapStateToProps(state) {
-    return { currentUser: state.currentUser.user };
-}
+// // export default Profile;
+// function mapStateToProps(state) {
+//     return { currentUser: state.currentUser.user };
+// }
 
-export default compose(
-    connect(mapStateToProps, actions)
-)(requireAuth(Profile));
+// export default compose(
+//     connect(mapStateToProps, actions)
+// )(requireAuth(Profile));
