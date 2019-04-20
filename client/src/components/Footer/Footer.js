cfea0440cc2d5123dce5cbb6a3b596e79ce8af57
Yusuf <ysserunjogi@gmail.com>
2019-04-20 10:41:08 -0700

    footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,11 +3,15 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import './footer.css';
+import { connect } from 'react-redux';
 
 class Footer extends React.Component {
     render() {
+        if (this.props.authenticated) {
+    
+
         return (
-            <div className="site-footer">
+            <div className="site-footer1">
                 <Container>
                     <Row>
                         <Col className=" md={8} xs={6} d-flex justify-content-center sm={6} ">
@@ -21,6 +25,24 @@ class Footer extends React.Component {
             </div>
         );
     }
+else {
+    return (
+<div className="site-footer2" >
+                <Container>
+                    <Row>
+                        <Col className=" md={8} xs={6} d-flex justify-content-center sm={6} ">
+                            © 2019 Wacky Waving Inflatable Flailing Arm Tube Men
+                        </Col>
+                        <Col className= " md={8} xs={6} d-flex justify-content-end sm={6} ">
+                            <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><i className="fas fa-code"></i></a>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }}
 }
-
-export default Footer;
+function mapStateToProps(state) {
+    return { authenticated: state.auth.authenticated };
+}
+export default connect(mapStateToProps)(Footer);
