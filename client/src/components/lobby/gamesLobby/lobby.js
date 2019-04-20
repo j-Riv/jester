7439a4dc1571d9fea737ef0995742d5c448df8dd
j-Riv <jose.alfredo.rivera@gmail.com>
2019-04-20 12:40:55 -0700

    fix

--- client/src/components/lobby/gamesLobby/lobby.js
+++ client/src/components/lobby/gamesLobby/lobby.js
@@ -26,9 +26,9 @@ class GamesLobby extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            profileOpen: false,
             showCreatedGame: false,
-            key: 'allGames',
-            profileOpen: false
+            key: 'allGames'
         }
     }
 
