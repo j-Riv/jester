50b92ba741c89da30754e90a4c5afcecec69d9fa
j-Riv <jose.alfredo.rivera@gmail.com>
2019-04-23 19:19:13 -0700

    Fix Lobby Tab --> All

--- client/src/containers/Lobby/GameTabs/All.js
+++ client/src/containers/Lobby/GameTabs/All.js
@@ -2,61 +2,47 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
+
 
 function All(props) {
     return (
-        <div className='mt-3'>
-            <Row className='px-3'>
-                <Col md={3}>
-                    <h4>
-                        King
-                    </h4>
-                </Col>
-                <Col md={6}>
-                    <h4>
-                        Room
-                    </h4>
-                </Col>
-                <Col md={3}>
-                    <h4>
-                        Category
-                    </h4>
-                </Col>
-            </Row>
+        <div>
             {props.peeps.map(item => {
                 return (
-                    <Card bg='dark' className='mb-3'>
-                        <Card.Body>
-                            <Row key={item._id}>
+                    <Row className="game-row" key={item._id}>
 
-                                <Col md={3} style={{ display: 'flex', justifyContent: 'start' }}>
+                        <Col md={3} style={{ display: 'flex', justifyContent: 'center' }}>
+                            <div>
+                                <h5>Creator: {item.username}</h5>
+                                {/* <Image src={item.image} roundedCircle /> */}
+                            </div>
+
+                        </Col>
+
+                        <Col md={6} style={{ display: 'flex', justifyContent: 'center' }}>
+                            <div>
+                                {item.users.length <= 5 ?
+                                    <Link to={`/room/${item._id}`}>
+                                        <h3>{item.game_name}</h3>
+                                        <p>{item.users.length}/{item.max_players}</p>
+                                    </Link>
+                                    :
                                     <div>
-                                        <p>{item.username}</p>
-                                        {/* <Image src={item.image} roundedCircle /> */}
+                                        <h3>{item.game_name}</h3>
+                                        <p>{item.users.length}/{item.max_players}</p>
                                     </div>
+                                }
+                            </div>
+                        </Col>
 
-                                </Col>
+                        <Col md={3} style={{ display: 'flex', justifyContent: 'center' }}>
+                            <div>
+                                <p>{item.category}</p>
+                                <p>{item.status}</p>
+                            </div>
+                        </Col>
 
-                                <Col md={6} style={{ display: 'flex', justifyContent: 'start' }}>
-                                    <div>
-                                        <Link to={`/room/${item._id}`}>
-                                            <p>{item.game_name} | {item.users.length}/{item.max_players}</p>
-                                            {/* <p>{item.users.length}/{item.max_players}</p> */}
-                                        </Link>
-                                    </div>
-
-                                </Col>
-
-                                <Col md={3} style={{ display: 'flex', justifyContent: 'start' }}>
-                                    <div>
-                                        <p>{item.category} | {item.status}</p>
-                                    </div>
-                                </Col>
-
-                            </Row>
-                        </Card.Body>
-                    </Card>
+                    </Row>
                 )
             })}
         </div>
